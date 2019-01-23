5136b2cee8b4e941646efb0435197c1b564081d2
Brian Flynn <bdflynny@gmail.com>
2019-01-23 00:32:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const drivers = ["Milo", "Otis", "Garfield"]
 
 const destructivelyAppendDriver = function(name){
   drivers.push(name)
+  console.log(drivers)
 }
 
 destructivelyAppendDriver('Ralph')
